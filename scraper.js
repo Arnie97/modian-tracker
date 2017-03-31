d0c4e7c683913e69e72abf8a32ea80c8494a4f86
Arnie97 <arnie97@gmail.com>
2017-03-31 14:57:00 +0800

    Refresh the page every 5 minutes.

--- scraper.js
+++ scraper.js
@@ -1,21 +1,38 @@
 'use strict';
 
 var page = require('webpage').create(),
+    previous = '',
     url = 'https://zhongchou.modian.com/item/8738.html';
 
 page.settings.loadImages = false;
 page.onConsoleMessage = function(msg) {
-    console.log(msg);
+    switch (msg[0]) {
+    case '#':
+        if (previous !== msg) {
+            previous = msg;
+            console.log(msg + new Date().toLocaleString());
+        }
+        break;
+    case '/':
+        break;
+    default:
+        console.log(msg);
+    }
 };
 
-page.open(url, function(status) {
-    if (status === 'success') {
-        page.evaluate(function() {
-            var lst = $('li.data>h2>span');
-            console.log(lst[0].textContent + ' ' + lst[1].textContent);
-        });
-        phantom.exit(0);
-    } else {
-      phantom.exit(1);
-    }
-});
+function refresh() {
+    page.open(url, function(status) {
+        if (status === 'success') {
+            page.evaluate(function() {
+                window.setTimeout(function() {
+                    var lst = $('li.data>h2>span');
+                    console.log('# ' + lst[0].textContent + ' ' + lst[1].textContent + ' ');
+                }, 5000);
+            });
+        } else {
+            console.log(status);
+        }
+    });
+}
+
+window.setInterval(refresh, 300000);
