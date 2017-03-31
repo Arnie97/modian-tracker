037b2c0cf75dc470e7580b7acb24bc9caa007b27
Arnie97 <arnie97@gmail.com>
2017-04-01 00:32:32 +0800

    Improve time display format.

--- scraper.js
+++ scraper.js
@@ -7,10 +7,10 @@ var page = require('webpage').create(),
 page.settings.loadImages = false;
 page.onConsoleMessage = function(msg) {
     switch (msg[0]) {
-    case '#':
+    case '\t':
         if (previous !== msg) {
             previous = msg;
-            console.log(msg + new Date().toLocaleString());
+            console.log(getTimeString() + msg);
         }
         break;
     case '/':
@@ -20,13 +20,22 @@ page.onConsoleMessage = function(msg) {
     }
 };
 
+function getTimeString() {
+    var date = new Date();
+    date.setTime(Date.now() + 8 * 3600 * 1000);
+    var str = date.toISOString().substr(0, 19).replace('T', ' ');
+    return str;
+}
+
 function refresh() {
     page.open(url, function(status) {
         if (status === 'success') {
             page.evaluate(function() {
                 window.setTimeout(function() {
-                    var lst = $('li.data>h2>span');
-                    console.log('# ' + lst[0].textContent + ' ' + lst[1].textContent + ' ');
+                    var lst = $('li.data>h2>span'),
+                        sum = lst[0].textContent.replace(',', ''),
+                        count = lst[1].textContent;
+                    console.log('\t' + sum + '\t' + count);
                 }, 5000);
             });
         } else {
@@ -35,4 +44,5 @@ function refresh() {
     });
 }
 
+refresh();
 window.setInterval(refresh, 300000);
