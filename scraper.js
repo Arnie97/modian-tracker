b8ee9abb441e8c906fcc62ae13552f86a75d8f6a
Arnie97 <arnie97@gmail.com>
2017-04-18 19:22:26 +0800

    Log console output in a text file.

--- scraper.js
+++ scraper.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var page = require('webpage').create();
+var fs = require('fs'),
+    page = require('webpage').create();
 
 page.settings.loadImages = false;
 page.onConsoleMessage = function(msg) {
     console.log(msg);
+    fs.write('moerail.txt', msg + '\n', 'a');
 };
 
 page.open('frame.html', function(status) {
